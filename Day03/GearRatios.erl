5384e93716f0f22185cda20ef8ff42459a2391ba
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2023-12-04 17:52:16 +0000

    Finally, that was so hard

--- Day03/GearRatios.erl
+++ Day03/GearRatios.erl
@@ -6,9 +6,17 @@ start() ->
     {ok, File} = file:open("Day03Input.txt",[read]),
     {ok, Lines} = file:read(File,1024 * 1024),
     Schematic = string:tokens(Lines, "\n"),            
+    %%% Part 1 %%%
     AllValidItems = get_all_valid_items([], Schematic, 1, length(Schematic)+1),
     TotalValidItems = sum(fun(X, Acc) -> X+Acc end, AllValidItems, 0),
     io:fwrite("Part 1 answer: ~p~n", [TotalValidItems]),
+    %%% Part 2 %%%
+    GearCoords = get_all_gear_coords([],Schematic, 1, length(Schematic)+1),
+    GearItemRows = get_all_gear_item_rows([],Schematic, GearCoords, 1, length(GearCoords)+1),
+    GearItems = get_all_gear_items([], Schematic, GearItemRows, 1, length(GearItemRows)+1),
+    GearPowers = get_powers([], GearItems, 1, length(GearItems)+1),
+    TotalGearPowers = sum(fun(X, Acc) -> X+Acc end, GearPowers, 0),
+    io:fwrite("Part 1 answer: ~p~n", [TotalGearPowers]),    
     ok.
 
 % Get the Schematic character at 1-indexed position (Row,Col)
@@ -100,7 +108,7 @@ get_item_by_coords(Output, Schematic, Row, CurrentIndex, StopIndex) ->
     NewOutput = string:concat(Output, Item),
     get_item_by_coords(NewOutput, Schematic, Row, CurrentIndex+1, StopIndex).
 
-% Check aLL surrounding squares to determine if a specific square is valid.
+% Check all surrounding squares to determine if a specific square is valid.
 is_item_square_valid(Schematic, Row, Col) ->
     TopLeft = is_special_character(get_schematic_item(Schematic, Row-1, Col-1)),
     TopMiddle = is_special_character(get_schematic_item(Schematic, Row-1, Col)),
@@ -139,7 +147,7 @@ get_valid_items_by_row(Output, Schematic, Row, RowList, CurrentIndex, StopIndex)
         end,
     get_valid_items_by_row(NewOutput, Schematic, Row, RowList, CurrentIndex+2, StopIndex).
 
-% Get all valid items in Schematoc.
+% Get all valid items in Schematic.
 get_all_valid_items(Output, _, StopIndex, StopIndex) ->
     Output;
 get_all_valid_items(Output, Schematic, CurrentIndex, StopIndex) ->
@@ -147,6 +155,202 @@ get_all_valid_items(Output, Schematic, CurrentIndex, StopIndex) ->
     ValidItems = get_valid_items_by_row([], Schematic, CurrentIndex, ItemLocations, 1, length(ItemLocations)+1),
     NewOutput = lists:concat([Output,ValidItems]),
     get_all_valid_items(NewOutput, Schematic, CurrentIndex+1, StopIndex).
+
+% Get the posiitons of all stars (gears) in a row.
+get_gear_positions(Output, _, _, StopIndex, StopIndex) ->
+    Output;
+get_gear_positions(Output, Schematic, Row, CurrentIndex, StopIndex) ->    
+    IsGear = is_gear(Schematic, Row, CurrentIndex),
+    NewOutput =
+        if             
+            IsGear ->
+                lists:concat([Output, [Row,CurrentIndex]]);
+            true ->
+                Output
+        end,
+    get_gear_positions(NewOutput, Schematic, Row, CurrentIndex+1, StopIndex).
+
+% Get the coordinates of all stars (gears) in schematic - row, col pairs.
+get_all_gear_coords(Output, _, StopIndex, StopIndex) ->
+    Output;
+get_all_gear_coords(Output, Schematic, CurrentIndex, StopIndex) ->
+    GearPositions = get_gear_positions([],Schematic, CurrentIndex, 1, get_row_Length(Schematic)+1),    
+    NewOutput = lists:concat([Output, GearPositions]),
+    get_all_gear_coords(NewOutput, Schematic, CurrentIndex+1, StopIndex).
+
+% Return true if star is a gear, false otherwise.
+is_gear(Schematic, Row, Col) ->
+    Item = get_schematic_item(Schematic, Row, Col),
+    IsStar = 
+        if Item == "*" ->
+            true;
+        true ->
+            false
+        end,
+    RowTopLeftFlag = is_a_number(get_schematic_item(Schematic, Row-1,Col-1)),
+    RowTopMiddleFlag = is_a_number(get_schematic_item(Schematic, Row-1,Col)),
+    RowTopRightFlag = is_a_number(get_schematic_item(Schematic, Row-1,Col+1)),
+    RowMiddleLeftFlag = is_a_number(get_schematic_item(Schematic, Row,Col-1)),
+    RowMiddleRightFlag = is_a_number(get_schematic_item(Schematic, Row,Col+1)),
+    RowBottomLeftFlag = is_a_number(get_schematic_item(Schematic, Row+1,Col-1)),
+    RowBottomMiddleFlag = is_a_number(get_schematic_item(Schematic, Row+1,Col)),
+    RowBottomRightFlag = is_a_number(get_schematic_item(Schematic, Row+1,Col+1)),
+    RowTopScoreTemp = 
+        if 
+            (RowTopLeftFlag and RowTopMiddleFlag and RowTopRightFlag) or
+            (RowTopLeftFlag and not RowTopMiddleFlag and not RowTopRightFlag) or
+            (not RowTopLeftFlag and not RowTopMiddleFlag and RowTopRightFlag) or
+            (RowTopLeftFlag and RowTopMiddleFlag and not RowTopRightFlag) or
+            (not RowTopLeftFlag and RowTopMiddleFlag and RowTopRightFlag) or
+            (not RowTopLeftFlag and RowTopMiddleFlag and not RowTopRightFlag) ->
+                1;
+            true ->
+                0
+        end,        
+    RowTopScore = 
+        if 
+            (RowTopLeftFlag and not RowTopMiddleFlag and RowTopRightFlag) ->
+                2;
+            true ->
+                RowTopScoreTemp
+        end,
+    RowMiddleScoreTemp =
+        if 
+            (RowMiddleLeftFlag and RowMiddleRightFlag) ->
+                2;
+            true ->
+                0
+        end,
+    RowMiddleScore =
+        if
+            (RowMiddleLeftFlag and (not RowMiddleRightFlag)) or 
+            ((not RowMiddleLeftFlag) and RowMiddleRightFlag) ->             
+                1;
+            true ->
+                RowMiddleScoreTemp
+        end,
+    RowBottomScoreTemp = 
+        if 
+            (RowBottomLeftFlag and RowBottomMiddleFlag and RowBottomRightFlag) or
+            (RowBottomLeftFlag and not RowBottomMiddleFlag and not RowBottomRightFlag) or
+            (not RowBottomLeftFlag and not RowBottomMiddleFlag and RowBottomRightFlag) or
+            (RowBottomLeftFlag and RowBottomMiddleFlag and not RowBottomRightFlag) or
+            (not RowBottomLeftFlag and RowBottomMiddleFlag and RowBottomRightFlag) or
+            (not RowBottomLeftFlag and RowBottomMiddleFlag and not RowBottomRightFlag) ->
+                1;
+            true ->
+                0
+        end,    
+    RowBottomScore =     
+        if 
+            (RowBottomLeftFlag and not RowBottomMiddleFlag and RowBottomRightFlag) ->                
+                2;
+            true ->
+                RowBottomScoreTemp
+        end,
+    TotalScore = RowTopScore + RowMiddleScore + RowBottomScore,
+    if 
+        (TotalScore == 2) and IsStar ->
+            true;
+        true ->
+            false
+    end.
+
+% Get associated gear item rows.
+get_gear_item_rows(Schematic, Row, Col) ->
+    RowTopLeftFlag = is_a_number(get_schematic_item(Schematic, Row-1,Col-1)),
+    RowTopMiddleFlag = is_a_number(get_schematic_item(Schematic, Row-1,Col)),
+    RowTopRightFlag = is_a_number(get_schematic_item(Schematic, Row-1,Col+1)),
+    RowMiddleLeftFlag = is_a_number(get_schematic_item(Schematic, Row,Col-1)),
+    RowMiddleRightFlag = is_a_number(get_schematic_item(Schematic, Row,Col+1)),
+    RowBottomLeftFlag = is_a_number(get_schematic_item(Schematic, Row+1,Col-1)),
+    RowBottomMiddleFlag = is_a_number(get_schematic_item(Schematic, Row+1,Col)),
+    RowBottomRightFlag = is_a_number(get_schematic_item(Schematic, Row+1,Col+1)),
+    Rows1 =
+        if  RowTopLeftFlag or RowTopMiddleFlag or RowTopRightFlag ->
+            [Row-1];
+        true ->
+            []
+        end,
+    Rows2 =
+        if  RowMiddleLeftFlag or RowMiddleRightFlag ->
+            lists:concat([Rows1,[Row]]);
+        true ->
+            Rows1
+        end,    
+    Rows3 =
+        if  RowBottomLeftFlag or RowBottomMiddleFlag or RowBottomRightFlag ->
+            lists:concat([Rows2,[Row+1]]);
+        true ->
+            Rows2
+        end,    
+    Rows3.
+
+% Get all gear items in supplied row.
+get_gear_items_in_row(Output, _, _, _, _, StopIndex, StopIndex) ->
+    Output;
+get_gear_items_in_row(Output, Schematic, Row, GearRow, GearCol, CurrentIndex, StopIndex) ->
+    ItemLocations = get_item_locations([],Schematic, Row,1,get_row_Length(Schematic)+1),
+    ItemLeft = lists:nth(CurrentIndex, ItemLocations),
+    ItemRight = lists:nth(CurrentIndex+1, ItemLocations),
+    NewOutput =
+        if 
+            (ItemLeft =< GearCol+1) and (ItemRight >= GearCol-1) ->
+                Item = get_item_by_coords("", Schematic, Row, ItemLeft, ItemRight), 
+                lists:concat([Output, [Item]]);
+            true ->
+                Output
+        end,
+    get_gear_items_in_row(NewOutput, Schematic, Row, GearRow, GearCol, CurrentIndex+2, StopIndex).
+
+% Get all gear item rows for all gear coords.
+get_all_gear_item_rows(Output, _, _, StopIndex, StopIndex) ->
+    Output;
+get_all_gear_item_rows(Output, Schematic, GearCoords, CurrentIndex, StopIndex) ->
+    GearRow = lists:nth(CurrentIndex, GearCoords),
+    GearCol = lists:nth(CurrentIndex+1, GearCoords),
+    ItemRows = get_gear_item_rows(Schematic,GearRow,GearCol),
+    NewOutput = lists:concat([Output, [[GearRow,GearCol],ItemRows]]),
+    get_all_gear_item_rows(NewOutput, Schematic, GearCoords, CurrentIndex+2, StopIndex).
+
+% Get all gear items from a gear item rows collection.
+get_all_gear_items(Output, _, _, StopIndex, StopIndex) ->
+    Output;
+get_all_gear_items(Output, Schematic, GearItemRows, CurrentIndex, StopIndex) ->
+    GearCoord = lists:nth(CurrentIndex, GearItemRows),
+    GearRow = lists:nth(1, GearCoord),
+    GearCol = lists:nth(2, GearCoord),
+    Rows = lists:nth(CurrentIndex+1, GearItemRows),    
+    NoRows = length(Rows),
+    GearItemsTotal =
+        if
+            NoRows == 1 ->
+                Row1 = lists:nth(1, Rows),                         
+                ItemLocations = get_item_locations([],Schematic, Row1,1,get_row_Length(Schematic)),
+                GearItems = get_gear_items_in_row([], Schematic, Row1, GearRow, GearCol, 1, length(ItemLocations)+1),
+                GearItems;
+            true ->
+                Row1 = lists:nth(1, Rows),
+                ItemLocations2 = get_item_locations([],Schematic, Row1,1,get_row_Length(Schematic)),
+                GearItems1 = get_gear_items_in_row([], Schematic, Row1, GearRow, GearCol, 1, length(ItemLocations2)+1),                
+                Row2 = lists:nth(2, Rows),
+                ItemLocations3 = get_item_locations([],Schematic, Row2,1,get_row_Length(Schematic)),
+                GearItems2 = get_gear_items_in_row([], Schematic, Row2, GearRow, GearCol, 1, length(ItemLocations3)+1),
+                lists:concat([GearItems1,GearItems2])
+        end,
+    NewOutput = lists:concat([Output, [GearItemsTotal]]),
+    get_all_gear_items(NewOutput, Schematic, GearItemRows, CurrentIndex+2, StopIndex).
+
+% Reduce gear list to powers.
+get_powers(Output, _, StopIndex, StopIndex) ->
+    Output;
+get_powers(Output, GearItems, CurrentIndex, StopIndex) ->
+    GearItem = lists:nth(CurrentIndex, GearItems),
+    FirstNum = lists:nth(1, GearItem),
+    SecondNum = lists:nth(2, GearItem),
+    Power = FirstNum * SecondNum,
+    NewOutput = lists:concat([Output, [Power]]),
+    get_powers(NewOutput, GearItems, CurrentIndex+1, StopIndex).
 
 % Generic sum function.
 sum(Func, Data, Acc) ->

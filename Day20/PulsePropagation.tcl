1f429a2c172392e46cd3cc7a90bca08be52841c5
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2024-01-13 16:23:14 +0000

    Attempt and fail to solve day 20 pt 2

--- Day20/PulsePropagation.tcl
+++ Day20/PulsePropagation.tcl
@@ -28,8 +28,15 @@ proc send {pulse sourceModule destModule modules memory} {
         set pulseStr "low"
     } else {
         set pulseStr "high"
-    }    
-    puts "$sourceModule -$pulseStr-> $destModule"
+    }        
+    # Get rx module for part 2.
+    set rxLowPulse 0
+    set rxHighPulse 0
+    if {$destModule == "rx" && $pulse == 0} {
+        set rxLowPulse 1 
+    } elseif {$destModule == "rx" && $pulse == 1} {
+        set rxHighPulse 1 
+    }
     # Flip-flop.
     if {[dict exists $modules "%$destModule"]} {        
         # Init newPulse.
@@ -42,7 +49,7 @@ proc send {pulse sourceModule destModule modules memory} {
         # If a high pulse is received, set memory but do nothing.
         if {$pulse == 1} {
             #puts "Processing hi pulse, no action"
-            return [list -1 {} $memory]
+            return [list -1 {} $memory $rxLowPulse $rxHighPulse]
         # If a low pulse is received flip-flop.
         } else {
             #puts "Processing low pulse"
@@ -56,7 +63,7 @@ proc send {pulse sourceModule destModule modules memory} {
                 set newPulse 1
             }
             set newDestModules [dict get $modules "%$destModule"]
-            return [list $newPulse $newDestModules $memory]
+            return [list $newPulse $newDestModules $memory $rxLowPulse $rxHighPulse]
         }        
     # Conjunction.
     } elseif {[dict exists $modules "&$destModule"]} {        
@@ -77,9 +84,9 @@ proc send {pulse sourceModule destModule modules memory} {
             }
         }        
         set newDestModules [dict get $modules "&$destModule"]        
-        return [list $newPulse $newDestModules $memory]
+        return [list $newPulse $newDestModules $memory $rxLowPulse $rxHighPulse]
     }
-    return [list $pulse {} $memory]
+    return [list $pulse {} $memory $rxLowPulse $rxHighPulse]
 }
 
 # Now process a button push, this will run until the pulses queue is empty.
@@ -87,13 +94,14 @@ proc pushButton {modules memory} {
     # Init high and low pulses.
     set noLowPulses 1
     set noHighPulses 0
+    set noRxLowPulses 0
+    set noRxHighPulses 0
     # Create an init pulses queue.
     set pulses [createQueue]
     set broadcasterModule [dict get $modules broadcaster]
     foreach module $broadcasterModule {
         enqueue pulses [list "broadcaster" $module 0]
     }
-    puts "button -low-> broadcaster"
     while {[llength $pulses] > 0} {
         # Get next module in queue.
         set nextModule [dequeue pulses]
@@ -110,22 +118,24 @@ proc pushButton {modules memory} {
         set newPulse [lindex $result 0]
         set newDestModules [lindex $result 1]
         set newMemory [lindex $result 2]
+        set rxlp [lindex $result 3]
+        set rxhp [lindex $result 4]
+        set noRxLowPulses [expr {$noRxLowPulses + $rxlp}]
+        set noRxHighPulses [expr {$noRxHighPulses + $rxhp}]
         # Add new pulses to queue and update memory.
         foreach module $newDestModules {
             enqueue pulses [list $destModule $module $newPulse]
         }
         set memory $newMemory
     }
-    return [list $memory $noLowPulses $noHighPulses]
+    return [list $memory $noLowPulses $noHighPulses $noRxLowPulses $noRxHighPulses]
 }
 
 # Now process a button push n times, this will run until the pulses queue is empty.
 proc pushButtonNTimes {modules memory n} {
     set totalNoLowPulses 0
-    set totalNoHighPulses 0    
+    set totalNoHighPulses 0
     for {set i 1} {$i <= $n} {incr i} {
-        puts ""
-        puts "Button push $i:"
         set result [pushButton $modules $memory]
         set newMemory [lindex $result 0]
         set noLowPulses [lindex $result 1]
@@ -135,6 +145,25 @@ proc pushButtonNTimes {modules memory n} {
         set memory $newMemory
     }
     return [list $memory $totalNoLowPulses $totalNoHighPulses]
+}
+
+# Try to find no pushes required to send a single low pulse to rx to switch on machine.
+proc findNoPushesToSwitchOn {modules memory} {
+    set i 0
+    while {true} {
+        set i [incr i]
+        set result [pushButton $modules $memory]
+        set newMemory [lindex $result 0]
+        set noLowPulses [lindex $result 1]
+        set noHighPulses [lindex $result 2]    
+        set noRxLowPulses [lindex $result 3]
+        set noRxHighPulses [lindex $result 4]
+        if {$noRxLowPulses >= 1} {
+            return $i
+        }
+        puts "$i: $noLowPulses, $noHighPulses, $noRxLowPulses, $noRxHighPulses" 
+        set memory $newMemory
+    }
 }
 
 # Initialise memory.
@@ -164,12 +193,13 @@ proc initMemory {modules} {
 # Modules which will contain the layout.
 set modules [parseInputFile "Day20Input.txt"]
 
-# Set uo memory.
+# Part 1.
 set memory [initMemory $modules]
-
-# Push the button a lot.
 set result [pushButtonNTimes $modules $memory 1000]
-
-# Get result.
 set pulseProduct [expr {[lindex $result 1] * [lindex $result 2]}]
 puts "Part 1 answer: $pulseProduct"
+
+# Part 2.
+#set memory [initMemory $modules]
+#set noPushes [findNoPushesToSwitchOn $modules $memory]
+#puts "Part 2 answer: $noPushes"

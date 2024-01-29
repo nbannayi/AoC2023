032086b8f6d702eff8b849592a1847961bd91f40
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2024-01-29 00:40:22 +0000

    Day 24 C# pt 1

---
+++ Day24/Hailstone.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace Day24
+{
+    /// <summary>
+    /// Represents a hailstone.
+    /// </summary>
+    public class Hailstone
+    {
+        private long _px, _py, _pz, _vx, _vy, _vz;
+        private double _gradient, _intercept;
+        public double Gradient { get { return _gradient; } }
+        public double Intercept { get { return _intercept; } }
+
+        // Instantiate a hailstone object.
+        public Hailstone(long px, long py, long pz, long vx, long vy, long vz)
+        {
+            _px = px; _py = py; _pz = pz; _vx = vx; _vy = vy; _vz = vz;
+            _gradient = (double)_vy / _vx;
+            _intercept = _py - _gradient * px;
+        }
+
+        // Get intersection between this and passed hailstone.
+        public (double, double) GetIntersection(Hailstone other)
+        {
+            var m1 = _gradient;
+            var c1 = _intercept;
+            var m2 = other.Gradient;
+            var c2 = other.Intercept;
+            var xi = (c2 - c1) / (m1 - m2);
+            var yi = m1 * xi + c1;
+            return (xi, yi);
+        }
+
+        // Determine if passed hailstone intersects with this one within given bounds.
+        public bool IntersectsWithinBounds(Hailstone other, (long,long) bounds)
+        {
+            var (ix,iy) = GetIntersection(other);
+            var (b1, b2) = bounds;
+            var boundsCheck = ix >= b1 && ix <= b2 && iy >= b1 && iy <= b2;
+            // Only check if in future if bounds check works.
+            if (boundsCheck)
+            {
+                var futureCheckThis =
+                    Math.Sign(ix - _px) == Math.Sign(_vx) &&
+                    Math.Sign(iy - _py) == Math.Sign(_vy);
+                var futureCheckOther =
+                    Math.Sign(ix - other._px) == Math.Sign(other._vx) &&
+                    Math.Sign(iy - other._py) == Math.Sign(other._vy);
+                return futureCheckThis && futureCheckOther;
+            }
+            return false;
+        }
+
+        // Output string representation.
+        public override string ToString()
+        {
+            return $"{_px}, {_py}, {_pz} @ {_vx}, {_vy}, {_vz}";
+        }
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Advent of Code 2023 - Day 24 - Never tell me the odds.
+/// C#
+/// </summary>
+namespace Day24
+{
+    class Program
+    {
+        // Parse input file and return all hailstones.
+        static List<Hailstone> ParseInput(string inputFile)
+        {            
+            string[] lines = File.ReadAllLines(inputFile);
+            var hailstones = new List<Hailstone>();
+            foreach (var line in lines)
+            {
+                var tokens = line.Replace('@', ',').Replace(" ", "").Split(',');
+                long[] inputs = Array.ConvertAll(tokens, long.Parse);
+                hailstones.Add(new Hailstone(inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5]));
+            }
+            return hailstones;
+        }
+
+        // Create a list containing all unique pairs of hailstones.
+        static List<(Hailstone,Hailstone)> GetHailstonePairs(List<Hailstone> hailstones)
+        {
+            List<(Hailstone, Hailstone)> pairs = new List<(Hailstone, Hailstone)>();
+            for (int i = 0; i < hailstones.Count - 1; i++)
+                for (int j = i + 1; j < hailstones.Count; j++)
+                    pairs.Add((hailstones[i], hailstones[j]));
+            return pairs;            
+        }
+
+        static void Main(string[] args)
+        {
+            // Parse input.
+            var inputFile = "Day24Input.txt";
+            var hailstonePairs = GetHailstonePairs(ParseInput(inputFile));
+
+            // Part 1.
+
+            // Work out if each pair intersects.
+            var bounds = (200000000000000, 400000000000000);
+            var noIntersections = 0;
+            foreach (var (hailstone1, hailstone2) in hailstonePairs)
+            {
+                var intersects = hailstone1.IntersectsWithinBounds(hailstone2, bounds);
+                if (intersects) noIntersections++;
+            }
+            Console.WriteLine("Part 1 answer: " + noIntersections);
+        }
+    }
+}
